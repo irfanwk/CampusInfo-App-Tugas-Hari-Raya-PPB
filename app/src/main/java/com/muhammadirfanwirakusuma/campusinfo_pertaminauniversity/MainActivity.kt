03b9229e3f042488fcdfb75c46cd92cfca6f2d8b
irfanwk <irfan.wk.2705@gmail.com>
2026-03-24 17:03:32 +0800

    first commit

---
+++ app/src/main/java/com/muhammadirfanwirakusuma/campusinfo_pertaminauniversity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.muhammadirfanwirakusuma.campusinfo_pertaminauniversity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.muhammadirfanwirakusuma.campusinfo_pertaminauniversity.ui.theme.CampusInfo_PertaminaUniversityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CampusInfo_PertaminaUniversityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CampusInfo_PertaminaUniversityTheme {
+        Greeting("Android")
+    }
+}
